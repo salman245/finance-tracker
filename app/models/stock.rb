3cf3c646a5818d5c78b8d7a61315b281035ec177
Salman Tahir <salmantahir3249@gmail.com>
2021-11-15 12:48:36 +0500

    AjaxRequest

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,9 +5,12 @@ class Stock < ApplicationRecord
     publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
     secret_token: 'Tpk_698fdc9e7adc4bb0b8636fd6a7f79a2d',
     endpoint: 'https://sandbox.iexapis.com/v1')
-    new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
-
+    
+    begin
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+    rescue => exception
+      return nil
+    end
   end
-
 end
 
