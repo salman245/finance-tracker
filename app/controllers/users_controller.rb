3ffd8b52dbf054f010fc59873cc567223d81b6ff
Salman Tahir <salmantahir3249@gmail.com>
2021-11-16 18:36:45 +0500

    Completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   def my_portfolio
+    @user = current_user
     @tracked_stocks = current_user.stocks
   end
 
@@ -8,9 +9,17 @@ class UsersController < ApplicationController
     @friends = current_user.friends
   end
 
+  def show
+    @user = User.find(params[:id])
+    @tracked_stocks = @user.stocks
+
+  end
+
   def search
     if params[:friend].present?
       @friends = User.search(params[:friend])
+      @friends = current_user.except_current_user(@friends)
+
       if @friends
         respond_to do |format|
           format.js { render partial: 'users/friend_result' }
