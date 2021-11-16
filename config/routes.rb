027eec1dc07cfe243309e2aa073010bcbd457150
Salman Tahir <salmantahir3249@gmail.com>
2021-11-16 16:34:55 +0500

    till281

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
 
+  resources :user_stocks, only: [:create, :destroy]
   get 'users/my_portfolio'
   devise_for :users
 root 'welcome#index'
 
 get 'my_portfolio', to: 'users#my_portfolio'
 get 'search_stock', to: 'stocks#search'
+get 'my_friends', to: 'users#my_friends'
+get 'search_friend', to: 'users#search'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
